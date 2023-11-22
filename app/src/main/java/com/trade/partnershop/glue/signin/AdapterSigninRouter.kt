01cb4d3b1d61539dc414b4177504dfd84f81492b
author-main <author-main@mail.ru>
2023-11-22 11:12:16 +0300

    Navigation 22-11-2023

--- app/src/main/java/com/trade/partnershop/glue/signin/AdapterSigninRouter.kt
+++ app/src/main/java/com/trade/partnershop/glue/signin/AdapterSigninRouter.kt
@@ -4,7 +4,13 @@ import com.trade.feature.signin.presentation.signin.SigninRouter
 
 class AdapterSigninRouter: SigninRouter {
     override fun launchSignup(email: String) {
+       /* val result: Try<Result> =
+            Try{HttpService.SendNotification(endpointUrl)}
 
+        when(result) {
+            is Success -> MarkNotificationAsSent()
+            is Failure    -> MarkNotificationAsNotSent()
+        }*/
     }
 
     override fun launchMain() {
