55948540ce3669e716b0348d6f0812c4d979535a
Admin <author-main@mail.ru>
2023-11-25 11:44:19 +0300

    24-11-2023

--- feature/signin/build.gradle.kts
+++ feature/signin/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.trade.feature.signin"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 26
@@ -34,7 +34,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
@@ -43,4 +43,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.fragment:fragment-ktx: 1.6.2")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 }
--- feature/signin/src/main/java/com/trade/feature/signin/presentation/signin/SigninFragment.kt
+++ feature/signin/src/main/java/com/trade/feature/signin/presentation/signin/SigninFragment.kt
@@ -10,23 +10,15 @@ import com.trade.feature.signin.R
 
 class SigninFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = SigninFragment()
-    }
+    //private val binding by viewBinding<FragmentSigninBinding>()
 
-    private lateinit var viewModel: SigninViewModel
+    private val viewModel by viewModels<SigninViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_signin, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SigninViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
